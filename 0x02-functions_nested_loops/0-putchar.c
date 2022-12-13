8aeff18847f2fd94ffed4d20e5728df457c42c11
henzjo <aschenakijo123@gmail.com>
2022-12-13 10:53:15 -0800

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,23 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
+ * main - print _putchar
+ * is written by henz jo
  * Return: always 0 (success)
  */
 
 int main(void)
 {
-	_putchar(" ");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
-	_putchar("\n");
-
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
 
 
