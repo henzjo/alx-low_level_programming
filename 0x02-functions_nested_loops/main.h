a741c5e1f1c1b81fdb4372ce6ef41954b9fa217a
henzjo <aschenakijo123@gmail.com>
2022-12-13 06:59:58 -0800

    m second c programming

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_h
+#define MAIN_h
+/**
+ * file main.h
+ * desc : header files cotains all the prototype functions used to 0x00 nested function in nested dir.
+ */
+int _putchar(char c);
+
+
+
+
+
+
+
+
+
+#endif
