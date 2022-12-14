175026537aaeb3dbc345c4dfe019ed04652b391c
henzjo <aschenakijo123@gmail.com>
2022-12-14 00:49:58 -0800

    task 1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,13 @@
 #ifndef MAIN_h
 #define MAIN_h
 /**
- * file main.h
+ * file - main.h
  * desc : header files cotains all the prototype functions used to 0x00 nested function in nested dir.
+ * Return: 0
+ *c:char for the letter
  */
 int _putchar(char c);
+void print_alphabet(void);
 
 
 
